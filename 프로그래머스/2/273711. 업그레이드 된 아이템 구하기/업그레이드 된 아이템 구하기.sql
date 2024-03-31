e8f91c31f8a5fd3d11afd5466c6804583d02ca47
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-04-01 01:10:40 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,3 @@
+
+select i.ITEM_ID, i.ITEM_NAME, i.RARITY from ITEM_INFO i inner join ITEM_TREE t on i.ITEM_ID = t.ITEM_ID where t.PARENT_ITEM_ID in (select ITEM_ID from ITEM_INFO where RARITY="RARE") order by i.ITEM_ID desc;
+

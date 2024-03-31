4c86c916568ec91d7d278944da0862cda777296b
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-03-31 13:45:38 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,1 @@
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, "%Y-%m-%d") as "DATE_OF_BIRTH" from MEMBER_PROFILE where date_format(DATE_OF_BIRTH, "%m") = 3 and (GENDER = 'W') and (TLNO is not NULL) order by MEMBER_ID;

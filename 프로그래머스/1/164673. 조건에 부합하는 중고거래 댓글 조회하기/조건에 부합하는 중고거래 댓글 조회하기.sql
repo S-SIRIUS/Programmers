85357a2a74fe8ead9a205b75b2aab8349a48c299
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-03-31 15:54:21 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,1 @@
+select b.TITLE, r.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, date_format(r.CREATED_DATE, "%Y-%m-%d") as "CREATED_DATE" from USED_GOODS_REPLY r inner join USED_GOODS_BOARD b on r.BOARD_ID = b.BOARD_ID where date_format(b.CREATED_DATE, "%m")=10 order by r.CREATED_DATE, b.TITLE;

620f79f6a932033e8835d582382ff9d2aa06c255
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-04-09 02:14:45 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,1 @@
+select sum(g.SCORE) as "SCORE", e.EMP_NO, e.EMP_NAME, e.POSITION, e.EMAIL from HR_DEPARTMENT d inner join HR_EMPLOYEES e on d.DEPT_ID = e.DEPT_ID inner join HR_GRADE g on e.EMP_NO = g.EMP_NO group by e.EMP_NO order by sum(g.SCORE) desc limit 1; 

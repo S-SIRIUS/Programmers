e2ce70ecce258669eb881693e0310b521bfb06d9
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-05-17 02:51:43 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,31 @@
+import math
+from collections import deque
+def solution(progresses, speeds):    
+    s_index = len(speeds)-1
+    d_time=[]
+    while progresses:
+        value = progresses.pop()
+        rest_time = 100 - value
+        distribute_time = math.ceil(rest_time / speeds[s_index])
+        d_time.append(distribute_time)
+        s_index-=1
+    
+    
+    count=0
+    answer=[]
+    
+    queue = deque()
+    new_d_time=[]
+    for i in d_time:
+        queue.appendleft(i)
+    print(queue)
+    
+    while queue:
+        standard=queue.popleft()
+        count=1
+        while queue and standard >= queue[0]:
+            count+=1
+            queue.popleft()
+        answer.append(count)
+    print(answer)
+    return answer

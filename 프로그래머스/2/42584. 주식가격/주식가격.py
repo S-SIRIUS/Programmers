2c9226ebfa37674d5cea72c2c4d6ecdb29c8f7f0
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-05-21 05:17:37 +0900

    [level 2] Title: 주식가격, Time: 34.78 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,15 +1,11 @@
 from collections import deque
 def solution(prices):
-    answer = []
-    
-    prices_queue=deque()
-    for price in prices:
-        prices_queue.append(price)
-
+    answer = []    
+    prices_queue=deque(prices)
     time=0
+    
     while prices_queue:
-        standard=prices_queue[0]
-        prices_queue.popleft()
+        standard=prices_queue.popleft()
         
         for price in prices_queue:
             if(standard <= price):

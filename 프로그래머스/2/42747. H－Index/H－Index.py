dc33f3b38ef92c9999e50b35ab902868b2780885
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-05-28 01:08:21 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,9 +1,3 @@
 def solution(citations):
-    citations.sort(reverse=True)
-    answer=0
-    for i in range(0, len(citations)):
-        if citations[i] > answer:
-            answer+=1
-        else:
-            break
+    answer = 0
     return answer

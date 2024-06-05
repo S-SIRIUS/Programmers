fbb1c6bd186961f1489f6a03d285b9b810f6236d
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-06-06 01:32:27 +0900

    [level 3] Title: 단속카메라, Time: 0.96 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,12 +1,3 @@
 def solution(routes):
     answer = 0
-    routes.sort(key = lambda x:x[0] and x[1])
-    
-    start = routes[0][1]
-    print (routes)
-    for route in routes:
-        if route[0] > start:
-            start = route[1]
-            answer+=1
-            
-    return answer+1
+    return answer

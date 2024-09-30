e834094521f7c69049e31b7d787971121bb40b02
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-09-30 21:06:48 +0900

    [level 2] Title: 배달, Time: 2.21 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,6 +1,6 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 30일 21:06:06
+2024년 09월 30일 21:06:47
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,39 +1,7 @@
-import sys
-import heapq
-input = sys.stdin.readline
-inf = int(1e9)
-
 def solution(N, road, K):
     answer = 0
-    graph=[[] for _ in range(N+1)]
-    
-    for i in road:
-        graph[i[0]].append((i[1], i[2]))
-        graph[i[1]].append((i[0], i[2]))
-    
-    distances=[inf]*(N+1)
-    
 
-    def dijkstra(start):
-        queue=[]
-        heapq.heappush(queue, (0, start))
-        distances[start]=0
-        while queue:
-            distance, node = heapq.heappop(queue)
-            
-            if distances[node] < distance:
-                continue
-                
-            for i in graph[node]:
-                cost = distance + i[1]
-                if cost < distances[i[0]]:
-                    distances[i[0]] = cost
-                    heapq.heappush(queue, (cost, i[0]))
-  
-    dijkstra(1)
-  
-    for i in distances:
-        if i<=K:
-            answer+=1
-  
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
     return answer

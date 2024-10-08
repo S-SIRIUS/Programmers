c1d63165bf8ac39ab7a5f87bf7b6f39fed01e610
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-10-08 19:39:59 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -0,0 +1,1 @@
+select p.MEMBER_NAME, r.REVIEW_TEXT, DATE_FORMAT(r.REVIEW_DATE, "%Y-%m-%d") from MEMBER_PROFILE p, (select * from REST_REVIEW where MEMBER_ID = (select MEMBER_ID from REST_REVIEW group by MEMBER_ID order by count(*) desc limit 1)) r where p.MEMBER_ID = r.MEMBER_ID order by r.REVIEW_DATE;

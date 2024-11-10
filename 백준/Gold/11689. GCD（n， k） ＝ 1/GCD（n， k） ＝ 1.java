df01c47b67593f1e011e2867651f6025b4f8906c
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-11-11 03:22:02 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 116 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long n = Long.parseLong(st.nextToken());
+        long result = n;
+
+        for(long i=2; i<=Math.sqrt(n); i++){
+            if(n%i==0){
+                result = result -(result/i);
+                while(n%i==0){
+                    n /=i;
+                }
+            }
+        }
+        if(n>1){
+            result = result - (result/n);
+        }
+        System.out.print(result);
+    }
+}

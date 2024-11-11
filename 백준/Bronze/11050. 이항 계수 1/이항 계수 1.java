5adce3f5d6f14152f1ada087a8b6d2fe15a73ec0
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-11-11 18:49:29 +0900

    [Bronze I] Title: 이항 계수 1, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,27 +1,24 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
+
         int [][] dp = new int [n+1][n+1];
 
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=i; j++){
-                if(j==1){
-                    dp[i][j]=i;
-                }
-                if(j==i || j==0){
-                    dp[i][j]=1;
-                }
-            }
+        for (int i=0; i<=n; i++){
+            dp[i][0]=1;
+            dp[i][1]=i%10007;
+            dp[i][i]=1;
         }
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=i; j++){
-                dp[i][j] = dp[i-1][j-1]+ dp[i-1][j];
+
+        for(int i=3; i<=n; i++){
+            for(int j=2; j<i; j++){
+                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%10007;
             }
         }
         System.out.print(dp[n][k]);

1efc21326b0293f3b1e9cb1810d21b917adff0d6
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2024-12-04 20:04:05 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,17 +1,15 @@
+# 퇴사(실버3)
 n = int(input())
-t=[]
-p=[]
-dp=[0] * (n+1)
-for i in range(n):
-    a, b = map(int, input().split())
-    t.append(a)
-    p.append(b)
+ans = [()]
+dp = [0]*(n+2)
+for i in range(1, n+1):
+    t, p = map(int, input().split())
+    ans.append((t,p))
 
-for i in range(0, n):
-    time = t[i]
-    profit = p[i]
-    if i + time <= n:
-        dp[i+time] = max(dp[i]+profit, dp[i+time])
-
-    dp[i+1] = max(dp[i], dp[i+1])
-print(dp[n])
+for i in range(1, n+1):
+    
+    dp[i] = max(dp[i], dp[i-1])
+    t, p = ans[i]
+    if i+t-1<=n:
+        dp[i+t] = max(dp[i+t], dp[i] + p)
+print(max(dp))

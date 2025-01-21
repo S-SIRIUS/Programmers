c72db84e816a46b30e6e11f4b27fc941387f27f2
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2025-01-21 19:12:12 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 204 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
@@ -0,0 +1,16 @@
+# GCD(n,k) = 1
+import math
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+result=n
+
+for i in range(2, int(math.sqrt(n)+1)):
+    if n %i==0:
+        result -= result/i
+        while n%i==0:
+            n/=i
+if n > 1:
+    result = result - (result/n)
+print(int(result))

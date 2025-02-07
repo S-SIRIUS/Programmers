f86378aa1fcdb37c231e62319c329dabe8a5374a
Cw Song <109223193+S-SIRIUS@users.noreply.github.com>
2025-02-07 21:12:21 +0900

    [Gold II] Title: 피보나치 수 6, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -0,0 +1,18 @@
+# 피보나치 6
+import sys
+sys.setrecursionlimit(10**8)
+input = sys.stdin.readline
+memo={}
+memo[0]=0
+memo[1]=1
+memo[2]=1
+
+def dp(i):
+    if i not in memo:
+        if i%2==0:
+            memo[i] = (dp(i//2)*(dp(i//2)+2*dp(i//2-1)))%1000000007
+        else:
+            memo[i] = (dp(i//2)**2 + dp(i//2+1)**2)%1000000007
+    return memo[i]
+n = int(input())
+print(dp(n))
